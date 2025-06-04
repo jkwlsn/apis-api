a1a2ef4acebe2ed8ff41c37a9c8d71bd7fbca4e7
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-04 17:19:43 +0100

    Refactors file based on Ruff warnings
    Moves from os.path to pathlib.Path.

--- lib/database_configuration.py
+++ lib/database_configuration.py
@@ -1,16 +1,17 @@
 """Read configuration values from .env.db file"""
 
-import os
+from dataclasses import dataclass
+from pathlib import Path
 
 from dotenv import dotenv_values
-from dataclasses import dataclass
 
 
 @dataclass
 class DatabaseConfiguration:
     def __init__(self, filename: str = ".env.db") -> None:
-        if os.path.exists(filename):
-            config: dict[str, str] | None = dotenv_values(filename)
+        file_path = Path(filename)
+        if Path.exists(file_path):
+            config: dict[str, str | None] = dotenv_values(file_path)
             self.host: str | None = config.get("POSTGRES_HOST")
             self.port: str | None = config.get("POSTGRES_PORT")
             self.dbname: str | None = config.get("POSTGRES_DB")

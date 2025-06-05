de63b36d93c36c5a8d4971bad6a7ddff100a5c18
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-05 17:00:43 +0100

    Adds `Hive` schema

---
+++ schemas/hive.py
@@ -0,0 +1,14 @@
+"""Hive schema"""
+
+from pydantic import BaseModel
+
+
+class HiveCreate(BaseModel):
+    name: str
+    apiary_id: int
+
+
+class HiveRead(BaseModel):
+    hive_id: int
+    name: str
+    apiary_id: int

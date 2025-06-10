d41797627fdaa32a2a5aa0336922ee0308210c99
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-10 17:39:18 +0100

    Adds method for `find_by_colony_id`

--- repositories/colony.py
+++ repositories/colony.py
@@ -15,3 +15,11 @@ class ColonyRepository:
         if results:
             return Colony(results[0]["colony_id"], hive_id)
         return None
+
+    def find_by_colony_id(self, colony_id: int) -> Colony | None:
+        query: str = "SELECT * FROM colonies WHERE colony_id = %s LIMIT 1;"
+        params: list = [colony_id]
+        results: list[dict] | None = self.db.execute(query, params)
+        if results:
+            return Colony(results[0]["colony_id"], results[0]["hive_id"])
+        return None

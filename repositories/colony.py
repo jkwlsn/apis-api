41d29ca86b5ebe0e6507584c0edce32d547af7f3
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-10 17:56:02 +0100

    Adds `delete` method

--- repositories/colony.py
+++ repositories/colony.py
@@ -48,3 +48,9 @@ class ColonyRepository:
             if results:
                 return Colony(results[0]["colony_id"], hive_id)
         return None
+
+    def delete(self, colony_id: int) -> bool:
+        query: str = "DELETE FROM colonies WHERE colony_id = %s"
+        params: list[int] = [colony_id]
+        results: list[dict] | None = self.db.execute(query, params)
+        return bool(results)

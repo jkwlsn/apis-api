ad7d04ce660c4a89deeaaa145e9d1f08526c5617
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-10 15:29:31 +0100

    Adds `find_by_apiary_id` method

--- repositories/apiary.py
+++ repositories/apiary.py
@@ -16,3 +16,16 @@ class ApiaryRepository:
             if results:
                 return Apiary(results[0]["apiary_id"], name, location, user_id)
         return None
+
+    def find_by_apiary_id(self, apiary_id: int) -> Apiary | None:
+        query: str = "SELECT * FROM apiaries WHERE apiary_id = %s LIMIT 1;"
+        params: list[int] = [apiary_id]
+        results: list[dict] | None = self.db.execute(query, params)
+        if results:
+            return Apiary(
+                results[0]["apiary_id"],
+                results[0]["name"],
+                results[0]["location"],
+                results[0]["user_id"],
+            )
+        return None

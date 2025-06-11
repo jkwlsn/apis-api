ee6934c6c73a45c4195f594864850ea1a69c8c4c
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-11 17:47:38 +0100

    Adds `find_by_action_id` method

--- repositories/action.py
+++ repositories/action.py
@@ -9,9 +9,21 @@ class ActionRepository:
         self.db: DatabaseConnection = db
 
     def create(self, notes: str, inspection_id: int) -> Action | None:
-        query = "INSERT INTO actions (notes, inspection_id) VALUES (%s, %s) RETURNING action_id;"
+        query: str = "INSERT INTO actions (notes, inspection_id) VALUES (%s, %s) RETURNING action_id;"
         params: list[str | int] = [notes, inspection_id]
         results: list[Action] | None = self.db.execute(query, params)
         if results:
             return Action(results[0]["action_id"], notes, inspection_id)
+        return None
+
+    def find_by_action_id(self, action_id: int) -> Action | None:
+        query: str = "SELECT * FROM actions WHERE action_id = %s LIMIT 1;"
+        params: list[int] = [action_id]
+        results: list[Action] | None = self.db.execute(query, params)
+        if results:
+            return Action(
+                results[0]["action_id"],
+                results[0]["notes"],
+                results[0]["inspection_id"],
+            )
         return None

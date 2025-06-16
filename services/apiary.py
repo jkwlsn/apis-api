c9f2aefdc8c433bafd45b36dbce3244cff5359c3
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-16 22:41:13 +0100

    Adds `create_apiary` method

---
+++ services/apiary.py
@@ -0,0 +1,33 @@
+"""Service class for handling Apiary operations"""
+
+from models.apiary import Apiary
+from repositories.apiary import ApiaryRepository
+from repositories.user import UserRepository
+
+
+class ApiaryService:
+    def __init__(
+        self, apiary_repo: ApiaryRepository, user_repo: UserRepository
+    ) -> None:
+        self.apiary_repo = apiary_repo
+        self.user_repo = user_repo
+        self.name_required = "Apiary name is required"
+        self.location_required = "Location is required"
+        self.invalid_user_id = "Invalid user_id"
+        self.invalid_apiary = "Invalid apiary_id"
+
+    def _validate_data(self, name: str, location: str, user_id: int) -> None:
+        if not name:
+            raise ValueError(self.name_required)
+        if not location:
+            raise ValueError(self.location_required)
+        if not isinstance(user_id, int) or user_id <= 0:
+            raise ValueError(self.invalid_user_id)
+
+    def create_apiary(self, name: str, location: str, user_id: int) -> Apiary | None:
+        name = name.strip()
+        location = location.strip()
+        self._validate_data(name, location, user_id)
+        if not self.user_repo.find_by_user_id(user_id):
+            raise ValueError(self.invalid_user_id)
+        return self.apiary_repo.create(name=name, location=location, user_id=user_id)

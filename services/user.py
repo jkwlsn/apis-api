a241dd86b0989ce7583a4a5138c0b1f00ac3b813
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-16 15:14:47 +0100

    Removes custom exceptions

--- services/user.py
+++ services/user.py
@@ -10,6 +10,10 @@ from utils.username_validator import UsernameValidator
 class UserService:
     def __init__(self, repo: UserRepository) -> None:
         self.repo: UserRepository = repo
+        self.username_invalid = "Username invalid"
+        self.password_invalid = "Password invalid"  # noqa: S105
+        self.username_taken = "Username already taken"
+        self.user_does_not_exist = "User does not exist"
 
     def create_user(self, username: str, password: str) -> User | None:
         """Creates a new user in the database if the user doesn't already exist"""
@@ -19,13 +23,13 @@ class UserService:
         normalised_username = username.strip().lower()
 
         if password_validator.validate(password) is False:
-            raise ValueError("Password invalid")
+            raise ValueError(self.password_invalid)
 
         if username_validator.validate(username) is False:
-            raise ValueError("Username invalid")
+            raise ValueError(self.username_invalid)
 
         if self.find_user_by_username(normalised_username):
-            raise ValueError("Username taken")
+            raise ValueError(self.username_taken)
 
         hashed_password = password_hasher.hash(password)
 
@@ -45,20 +49,20 @@ class UserService:
         hashed_password = password_hasher.hash(password)
 
         if username_validator.validate(username) is False:
-            raise ValueError("Username invalid")
+            raise ValueError(self.username_invalid)
 
         if password_validator.validate(password) is False:
-            raise ValueError("Password invalid")
+            raise ValueError(self.password_invalid)
 
         user_exists = self.find_user_by_user_id(user_id)
 
         if user_exists is None:
-            raise ValueError("User does not exist")
+            raise ValueError(self.user_does_not_exist)
 
         username_exists = self.find_user_by_username(normalised_username)
 
         if username_exists and (username_exists.user_id != user_exists.user_id):
-            raise ValueError("Username taken")
+            raise ValueError(self.username_taken)
 
         return self.repo.update(
             user_id=user_id, username=normalised_username, password=hashed_password

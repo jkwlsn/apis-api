aa1ba481a6e591d9fa0ba1cd9728023d6af2c6cd
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-17 12:32:15 +0100

    Adds tests for `find_colony_by_colony_id` method

--- tests/test_colony_service.py
+++ tests/test_colony_service.py
@@ -64,3 +64,37 @@ def test_can_not_create_colony_missing_hive_id(
 
     with pytest.raises(ValueError, match="Invalid hive_id"):
         colony_service.create_colony(hive_id)
+
+
+def test_find_colony_by_colony_id(
+    colony_repo: MagicMock, hive_repo: MagicMock, test_data: Colony
+) -> None:
+    colony_repo.find_by_colony_id.return_value = test_data
+    colony_service: ColonyService = ColonyService(colony_repo, hive_repo)
+
+    results: Colony | None = colony_service.find_colony_by_colony_id(1)
+
+    assert isinstance(results, Colony)
+    assert results.colony_id == test_data.colony_id
+    assert results.hive_id == test_data.hive_id
+
+
+def test_can_not_find_colony_by_missing_colony_id(
+    colony_repo: MagicMock, hive_repo: MagicMock
+) -> None:
+    colony_repo.find_by_colony_id.return_value = None
+    colony_service: ColonyService = ColonyService(colony_repo, hive_repo)
+
+    result = colony_service.find_colony_by_colony_id(999)
+
+    assert result is None
+
+
+def test_can_not_find_colony_by_invalid_colony_id(
+    colony_repo: MagicMock, hive_repo: MagicMock
+) -> None:
+    colony_repo.find_by_colony_id.return_value = None
+    colony_service: ColonyService = ColonyService(colony_repo, hive_repo)
+
+    with pytest.raises(ValueError, match="Invalid colony_id"):
+        colony_service.find_colony_by_colony_id(-1)

70ce5b09fbc205c09663d4aeefe41a24c260f800
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-17 13:36:57 +0100

    Adds tests for `delete_queen` method

--- tests/test_queen_service.py
+++ tests/test_queen_service.py
@@ -156,3 +156,125 @@ def test_can_not_find_queen_by_invalid_colony_id(
 
     with pytest.raises(ValueError, match="Invalid colony_id"):
         queen_service.find_queen_by_colony_id(colony_id)
+
+
+def test_update_queen(
+    queen_repo: MagicMock, colony_repo: MagicMock, test_data: Queen
+) -> None:
+    queen_id = 1
+    colour = "Yellow"
+    clipped = True
+    colony_id = 1
+    queen_repo.find_by_queen_id.return_value = test_data
+    colony_repo.find_by_colony_id.return_value = Colony(1, 1)
+    queen_repo.update.return_value = test_data
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    results: Queen | None = queen_service.update_queen(
+        queen_id=queen_id, colour=colour, clipped=clipped, colony_id=colony_id
+    )
+
+    assert results.queen_id == test_data.queen_id
+    assert results.colour == test_data.colour
+    assert results.clipped is True
+    assert results.colony_id == test_data.colony_id
+
+
+def test_can_not_update_queen_invalid_queen_id(
+    queen_repo: MagicMock, colony_repo: MagicMock, test_data: Queen
+) -> None:
+    queen_repo.update.return_value = test_data
+    queen_id = -1
+    colour = "Yellow"
+    clipped = True
+    colony_id = 1
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    with pytest.raises(ValueError, match="Invalid queen_id"):
+        queen_service.update_queen(
+            queen_id=queen_id, colour=colour, clipped=clipped, colony_id=colony_id
+        )
+
+
+def test_can_not_update_queen_missing_queen_id(
+    queen_repo: MagicMock, colony_repo: MagicMock
+) -> None:
+    queen_repo.find_by_queen_id.return_value = None
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+    queen_id = 999
+    colour = "Yellow"
+    clipped = True
+    colony_id = 1
+
+    with pytest.raises(ValueError, match="Invalid queen_id"):
+        queen_service.update_queen(
+            queen_id=queen_id, colour=colour, clipped=clipped, colony_id=colony_id
+        )
+
+
+def test_can_not_update_queen_invalid_colony_id(
+    queen_repo: MagicMock, colony_repo: MagicMock, test_data: Queen
+) -> None:
+    queen_repo.update.return_value = test_data
+    queen_id = 1
+    colour = "Yellow"
+    clipped = True
+    colony_id = -1
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    with pytest.raises(ValueError, match="Invalid colony_id"):
+        queen_service.update_queen(
+            queen_id=queen_id, colour=colour, clipped=clipped, colony_id=colony_id
+        )
+
+
+def test_can_not_update_queen_missing_colony_id(
+    queen_repo: MagicMock, colony_repo: MagicMock, test_data: Queen
+) -> None:
+    queen_repo.update.return_value = test_data
+    colony_repo.find_by_colony_id.return_value = None
+    queen_id = 1
+    colour = "Yellow"
+    clipped = True
+    colony_id = 999
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    with pytest.raises(ValueError, match="Invalid colony_id"):
+        queen_service.update_queen(
+            queen_id=queen_id, colour=colour, clipped=clipped, colony_id=colony_id
+        )
+
+
+def test_delete_queen(queen_repo: MagicMock, colony_repo: MagicMock) -> None:
+    queen_id = 1
+    queen_repo.delete.return_value = True
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    result: bool = queen_service.delete_queen(queen_id)
+
+    assert result is True
+
+
+def test_can_not_delete_queen_missing_queen_id(
+    queen_repo: MagicMock, colony_repo: MagicMock
+) -> None:
+    queen_id = 999
+    queen_repo.find_by_queen_id.return_value = None
+    queen_repo.delete.return_value = False
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    results: bool = queen_service.delete_queen(queen_id)
+
+    assert results is False
+
+
+def test_can_not_delete_queen_invalid_queen_id(
+    queen_repo: MagicMock, colony_repo: MagicMock
+) -> None:
+    queen_id = -1
+    queen_repo.delete.return_value = False
+    queen_repo.find_by_queen_id.return_value = None
+    queen_service: QueenService = QueenService(queen_repo, colony_repo)
+
+    with pytest.raises(ValueError, match="Invalid queen_id"):
+        queen_service.delete_queen(queen_id)

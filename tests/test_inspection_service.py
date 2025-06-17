567fe5d40d4fc68f45e006ff04dddb35a2237486
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-17 14:45:28 +0100

    Adds tests for `create_inspection` method

---
+++ tests/test_inspection_service.py
@@ -0,0 +1,102 @@
+"""Tests for InspectionService"""
+
+from datetime import datetime
+from unittest.mock import MagicMock
+from zoneinfo import ZoneInfo
+
+import pytest
+
+from models.colony import Colony
+from models.inspection import Inspection
+from services.inspection import InspectionService
+
+
+@pytest.fixture
+def inspection_repo() -> MagicMock:
+    return MagicMock()
+
+
+@pytest.fixture
+def colony_repo() -> MagicMock:
+    return MagicMock()
+
+
+@pytest.fixture
+def test_data() -> Inspection:
+    inspection_timestamp = datetime(2020, 6, 23, 2, 10, 25, tzinfo=ZoneInfo("Etc/UTC"))
+    return Inspection(
+        inspection_id=1, inspection_timestamp=inspection_timestamp, colony_id=1
+    )
+
+
+@pytest.fixture
+def test_data_2() -> Inspection:
+    inspection_timestamp = datetime(2020, 6, 23, 2, 10, 25, tzinfo=ZoneInfo("Etc/UTC"))
+    return Inspection(
+        inspection_id=2, inspection_timestamp=inspection_timestamp, colony_id=2
+    )
+
+
+def test_create_inspection(
+    inspection_repo: MagicMock, colony_repo: MagicMock, test_data: Inspection
+) -> None:
+    inspection_repo.create.return_value = test_data
+    inspection_timestamp = datetime(2020, 6, 23, 2, 10, 25, tzinfo=ZoneInfo("Etc/UTC"))
+    colony_id = 1
+    inspection_service: InspectionService = InspectionService(
+        inspection_repo, colony_repo
+    )
+
+    results: Inspection | None = inspection_service.create_inspection(
+        inspection_timestamp=inspection_timestamp, colony_id=colony_id
+    )
+
+    assert isinstance(results, Inspection)
+    assert results.inspection_id == test_data.inspection_id
+    assert results.inspection_timestamp == test_data.inspection_timestamp
+    assert results.colony_id == test_data.colony_id
+
+
+def test_can_not_create_inspection_invalid_inspection_timestamp(
+    inspection_repo: MagicMock, colony_repo: MagicMock
+) -> None:
+    inspection_timestamp = "2025-06-10"
+    colony_id = 1
+    inspection_service: InspectionService = InspectionService(
+        inspection_repo, colony_repo
+    )
+
+    with pytest.raises(TypeError, match="Invalid inspection_timestamp"):
+        inspection_service.create_inspection(
+            inspection_timestamp=inspection_timestamp,
+            colony_id=colony_id,
+        )
+
+
+def test_can_not_create_inspection_invalid_colony_id(
+    inspection_repo: MagicMock, colony_repo: MagicMock, test_data: Inspection
+) -> None:
+    inspection_repo.create.return_value = test_data
+    colony_id = -1
+    inspection_timestamp = datetime(2020, 6, 23, 2, 10, 25, tzinfo=ZoneInfo("Etc/UTC"))
+    inspection_service: InspectionService = InspectionService(
+        inspection_repo, colony_repo
+    )
+
+    with pytest.raises(ValueError, match="Invalid colony_id"):
+        inspection_service.create_inspection(inspection_timestamp, colony_id)
+
+
+def test_can_not_create_inspection_missing_colony_id(
+    inspection_repo: MagicMock, colony_repo: MagicMock, test_data: Inspection
+) -> None:
+    inspection_repo.create.return_value = test_data
+    colony_repo.find_by_colony_id.return_value = None
+    inspection_timestamp = datetime(2020, 6, 23, 2, 10, 25, tzinfo=ZoneInfo("Etc/UTC"))
+    colony_id = 999
+    inspection_service: InspectionService = InspectionService(
+        inspection_repo, colony_repo
+    )
+
+    with pytest.raises(ValueError, match="Invalid colony_id"):
+        inspection_service.create_inspection(inspection_timestamp, colony_id)

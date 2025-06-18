2382479e0b7bb5015bd8b0ee993f5c18ba8e7e20
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-06-18 14:00:29 +0100

    Adds tests for `update_observation` method

--- tests/test_observation_service.py
+++ tests/test_observation_service.py
@@ -274,3 +274,234 @@ def test_can_not_find_observation_by_invalid_inspection_id(
 
     with pytest.raises(ValueError, match="Invalid inspection_id"):
         observation_service.find_observation_by_inspection_id(inspection_id)
+
+
+def test_update_observation(
+    observation_repo: MagicMock, inspection_repo: MagicMock, test_data: Observation
+) -> None:
+    observation_id = 1
+    queenright = True
+    queen_cells = 5
+    bias = True
+    brood_frames = 5
+    store_frames = 6
+    chalk_brood = False
+    foul_brood = False
+    varroa_count = 10
+    temper = 5
+    notes = "Example notes"
+    inspection_id = 1
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+    observation_repo.find_by_observation_id.return_value = test_data
+    inspection_repo.find_by_inspection_id.return_value = Inspection(
+        1, datetime(2025, 10, 1, 10, 30, 1, 00, tzinfo=ZoneInfo("Etc/UTC")), 1
+    )
+    observation_repo.update.return_value = test_data
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+
+    results: Observation | None = observation_service.update_observation(
+        observation_id=observation_id,
+        queenright=queenright,
+        queen_cells=queen_cells,
+        bias=bias,
+        brood_frames=brood_frames,
+        store_frames=store_frames,
+        chalk_brood=chalk_brood,
+        foul_brood=foul_brood,
+        varroa_count=varroa_count,
+        temper=temper,
+        notes=notes,
+        inspection_id=inspection_id,
+    )
+
+    assert results.observation_id == test_data.observation_id
+    assert results.inspection_id == test_data.inspection_id
+
+
+def test_can_not_update_observation_invalid_observation_id(
+    observation_repo: MagicMock, inspection_repo: MagicMock, test_data: Observation
+) -> None:
+    observation_repo.update.return_value = test_data
+    observation_id = -1
+    queenright = True
+    queen_cells = 5
+    bias = True
+    brood_frames = 5
+    store_frames = 6
+    chalk_brood = False
+    foul_brood = False
+    varroa_count = 10
+    temper = 5
+    notes = "Example notes"
+    inspection_id = 1
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+
+    with pytest.raises(ValueError, match="Invalid observation_id"):
+        observation_service.update_observation(
+            observation_id=observation_id,
+            queenright=queenright,
+            queen_cells=queen_cells,
+            bias=bias,
+            brood_frames=brood_frames,
+            store_frames=store_frames,
+            chalk_brood=chalk_brood,
+            foul_brood=foul_brood,
+            varroa_count=varroa_count,
+            temper=temper,
+            notes=notes,
+            inspection_id=inspection_id,
+        )
+
+
+def test_can_not_update_observation_missing_observation_id(
+    observation_repo: MagicMock, inspection_repo: MagicMock
+) -> None:
+    observation_id = 1
+    queenright = True
+    queen_cells = 5
+    bias = True
+    brood_frames = 5
+    store_frames = 6
+    chalk_brood = False
+    foul_brood = False
+    varroa_count = 10
+    temper = 5
+    notes = "Example notes"
+    inspection_id = 1
+    observation_repo.find_by_observation_id.return_value = None
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+
+    with pytest.raises(ValueError, match="Invalid observation_id"):
+        observation_service.update_observation(
+            observation_id=observation_id,
+            queenright=queenright,
+            queen_cells=queen_cells,
+            bias=bias,
+            brood_frames=brood_frames,
+            store_frames=store_frames,
+            chalk_brood=chalk_brood,
+            foul_brood=foul_brood,
+            varroa_count=varroa_count,
+            temper=temper,
+            notes=notes,
+            inspection_id=inspection_id,
+        )
+
+
+def test_can_not_update_observation_invalid_notes(
+    observation_repo: MagicMock, inspection_repo: MagicMock
+) -> None:
+    observation_id = 1
+    queenright = True
+    queen_cells = 5
+    bias = True
+    brood_frames = 5
+    store_frames = 6
+    chalk_brood = False
+    foul_brood = False
+    varroa_count = 10
+    temper = 5
+    notes = 9999
+    inspection_id = 1
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+
+    with pytest.raises(TypeError, match="Invalid notes"):
+        observation_service.update_observation(
+            observation_id=observation_id,
+            queenright=queenright,
+            queen_cells=queen_cells,
+            bias=bias,
+            brood_frames=brood_frames,
+            store_frames=store_frames,
+            chalk_brood=chalk_brood,
+            foul_brood=foul_brood,
+            varroa_count=varroa_count,
+            temper=temper,
+            notes=notes,
+            inspection_id=inspection_id,
+        )
+
+
+def test_can_not_update_observation_invalid_inspection_id(
+    observation_repo: MagicMock, inspection_repo: MagicMock, test_data: Observation
+) -> None:
+    observation_repo.update.return_value = test_data
+    observation_id = 1
+    queenright = True
+    queen_cells = 5
+    bias = True
+    brood_frames = 5
+    store_frames = 6
+    chalk_brood = False
+    foul_brood = False
+    varroa_count = 10
+    temper = 5
+    notes = "Example note"
+    inspection_id = -1
+    inspection_repo.find_by_inspection_id.return_value = None
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+
+    with pytest.raises(ValueError, match="Invalid inspection_id"):
+        observation_service.update_observation(
+            observation_id=observation_id,
+            queenright=queenright,
+            queen_cells=queen_cells,
+            bias=bias,
+            brood_frames=brood_frames,
+            store_frames=store_frames,
+            chalk_brood=chalk_brood,
+            foul_brood=foul_brood,
+            varroa_count=varroa_count,
+            temper=temper,
+            notes=notes,
+            inspection_id=inspection_id,
+        )
+
+
+def test_can_not_update_observation_missing_inspection_id(
+    observation_repo: MagicMock, inspection_repo: MagicMock
+) -> None:
+    observation_id = 1
+    queenright = True
+    queen_cells = 5
+    bias = True
+    brood_frames = 5
+    store_frames = 6
+    chalk_brood = False
+    foul_brood = False
+    varroa_count = 10
+    temper = 5
+    notes = "Example note"
+    inspection_id = 999
+    inspection_repo.find_by_inspection_id.return_value = None
+    observation_service: ObservationService = ObservationService(
+        observation_repo, inspection_repo
+    )
+
+    with pytest.raises(ValueError, match="Invalid inspection_id"):
+        observation_service.update_observation(
+            observation_id=observation_id,
+            queenright=queenright,
+            queen_cells=queen_cells,
+            bias=bias,
+            brood_frames=brood_frames,
+            store_frames=store_frames,
+            chalk_brood=chalk_brood,
+            foul_brood=foul_brood,
+            varroa_count=varroa_count,
+            temper=temper,
+            notes=notes,
+            inspection_id=inspection_id,
+        )

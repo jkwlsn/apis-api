fc4e00e496b4df9068570fc8cbdeb329a0d80a1c
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-07-01 12:23:29 +0100

    Adds queen router to main

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from db.instance import db
 from routes.apiary import router as apiary_router
 from routes.colony import router as colony_router
 from routes.hive import router as hive_router
+from routes.queen import router as queen_router
 from routes.user import router as user_router
 
 
@@ -27,3 +28,4 @@ app.include_router(user_router)
 app.include_router(apiary_router)
 app.include_router(hive_router)
 app.include_router(colony_router)
+app.include_router(queen_router)

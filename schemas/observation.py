ac6ee2aef870162520268f4c856dd54b3dce07bf
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2025-07-02 13:32:20 +0100

    Adds ObservationUpdate to schema

--- schemas/observation.py
+++ schemas/observation.py
@@ -17,6 +17,20 @@ class ObservationCreate(BaseModel):
     inspection_id: int
 
 
+class ObservationUpdate(BaseModel):
+    queenright: bool
+    queen_cells: int
+    bias: bool
+    brood_frames: int
+    store_frames: int
+    chalk_brood: bool
+    foul_brood: bool
+    varroa_count: int
+    temper: int
+    notes: str
+    inspection_id: int
+
+
 class ObservationRead(BaseModel):
     observation_id: int
     queenright: bool
